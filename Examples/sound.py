162e49531010565d69db247fde2dc35f8a96d4a4
Manh Nguyen <108793626+TManhNguyen@users.noreply.github.com>
2022-12-08 15:41:46 +0700

    Change gpio port sound sensor

--- Examples/sound.py
+++ Examples/sound.py
@@ -1,28 +1,28 @@
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
 import time
 
-#GPIO SETUP 
-channel = 17 
+#GPIO SETUP
+channel = 20 
 
-GPIO.setmode(GPIO.BCM) 
+GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(channel, GPIO.IN) 
+GPIO.setup(channel, GPIO.IN)
 
-def callback(channel): 
+def callback(channel):
 
-    if GPIO.input(channel): 
+    if GPIO.input(channel):
 
-            print ("Sound Detected!") 
+            print ("Sound Detected!")
 
-    else: 
+    else:
 
             print ("Sound Detected!" )
 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW 
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change 
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
 
-# infinite loop 
+# infinite loop
 
 while True:
     time.sleep(1)
