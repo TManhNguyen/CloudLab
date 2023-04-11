d7ec538694d74e038e35b0405808756c8e13e50a
Manh Nguyen <108793626+TManhNguyen@users.noreply.github.com>
2023-04-11 14:23:41 +0700

    adding tiltswitch example

---
+++ Examples/TiltSwitch.py
@@ -0,0 +1,77 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+
+import time
+from grove.gpio import GPIO
+
+
+class GroveTiltSwitch(GPIO):
+    def __init__(self, pin):
+        super(GroveTiltSwitch, self).__init__(pin, GPIO.IN)
+        self._on_trigger = None
+        self._on_release = None
+
+    @property
+    def on_trigger(self):
+        return self._on_trigger
+
+    @on_trigger.setter
+    def on_trigger(self, callback):
+        if not callable(callback):
+            return
+
+        if self.on_event is None:
+            self.on_event = self._handle_event
+
+        self._on_trigger = callback
+
+    @property
+    def on_release(self):
+        return self._on_release
+
+    @on_release.setter
+    def on_release(self, callback):
+        if not callable(callback):
+            return
+
+        if self.on_event is None:
+            self.on_event = self._handle_event
+
+        self._on_release = callback
+
+    def _handle_event(self, pin, value):
+
+        if value:
+            if callable(self._on_trigger):
+                self._on_trigger()
+        else:
+            if callable(self._on_release):
+                self._on_release()
+
+Grove = GroveTiltSwitch
+
+
+def main():
+    import sys
+
+    if len(sys.argv) < 2:
+        print('Usage: {} pin'.format(sys.argv[0]))
+        sys.exit(1)
+
+    swicth = GroveTiltSwitch(int(sys.argv[1]))
+
+    def on_trigger():
+        print('Triggered')
+    def on_release():
+        print("Released.")
+
+    swicth.on_trigger = on_trigger
+    swicth.on_release = on_release
+
+    while True:
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
